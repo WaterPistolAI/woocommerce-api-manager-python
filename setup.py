d12c99feaabe9b2978bfc75b31276a0039fd63f3
unclemusclez <unclemusclez@gmail.com>
2025-07-18 19:35:54 -0400

    update version, migrate to Water Pistol

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ print("Found packages:")
 pprint.pprint(packages)
 setup(
     name='woocommerce-api-manager',
-    version='0.6.1',
+    version='0.7.0',
     description='Python Library for WooCommerce Kestrel API Manager',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Devin J. Dawson',
     author_email='devin@waterpistol.co',
-    url='https://github.com/unclemusclez/wc-api-manager-python',
+    url='https://github.com/waterpistolai/woocommerce-api-manager-python',
     packages=packages,
     install_requires=[
         'woocommerce'
